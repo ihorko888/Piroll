9f2f291c0cf3bce2eb979436ec32a28aa69ed2fe
Ihor <kolotyuk@gmail.com>
2020-12-14 15:52:52 +0200

    fixed nav-menu and added skillBar section

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -28,4 +28,21 @@ $(function () {
     	body.classList.add('mouse');
     }
 
+    // Skill Bar
+    $(window).scroll(function() {
+        var hT = $('#skill-bar-wrapper').offset().top,
+            hH = $('#skill-bar-wrapper').outerHeight(),
+            wH = $(window).height(),
+            wS = $(this).scrollTop();
+        if (wS > (hT+hH-1.4*wH)){
+            jQuery(document).ready(function(){
+                jQuery('.skillbar-container').each(function(){
+                    jQuery(this).find('.skill').animate({
+                        width:jQuery(this).attr('data-percent')
+                    }, 2000);
+                });
+            });
+        }
+     });
+
 });
