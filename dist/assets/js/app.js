cec4832bde7ef4845ff12d3de62fa9144e15bc08
Ihor <kolotyuk@gmail.com>
2020-12-17 18:22:52 +0200

    Added scroll button top

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -4323,4 +4323,20 @@ $(function () {
         };
     }
 
+    // scrollTopBtn
+    $(document).ready(function() {
+    
+        $(window).scroll(function() {
+            if($(this).scrollTop() > 40){
+                $('.topBtn').fadeIn();
+            } else {
+                $('.topBtn').fadeOut();
+            }
+        });
+    
+        $('.topBtn').click(function() {
+            $('html, body').animate({scrollTop: 0},500);
+        });
+    });
+
 });
