efe8e2f1fb9288fcceb571ca637856768ac970ff
Ihor <kolotyuk@gmail.com>
2020-12-19 23:08:30 +0200

    Added adaptive header and burger menu

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -4339,4 +4339,21 @@ $(function () {
         });
     });
 
+    let navToggle = $('#navToggle');   
+    let nav = $('#nav');
+    
+    navToggle.on('click', function(event) {
+        event.preventDefault();
+    
+        $("body").toggleClass('show-nav');
+        $(this).toggleClass('active');
+        nav.toggleClass('show');
+    });
+    
+    $(window).on('resize', function () {
+        $("body").removeClass('show-nav');
+        navToggle.removeClass('active');
+        nav.removeClass('show');
+    });
+
 });
